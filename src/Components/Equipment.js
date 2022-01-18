5c7fbbf979464761a6424c6e27303ee8ae3ddc20
Nate Ewert-Krocker <n.ewertkrocker@gmail.com>
2022-01-18 12:27:44 -0500

    Add 'selected' class to equipment components

--- src/Components/Equipment.js
+++ src/Components/Equipment.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import '../css/Equipment.css';
 import '../Assets/item_icon_checked.png';
 import discardIcon from '../Assets/delete.png';
 
 const Equipment = ({ id, dye, type, name, icon, saveItem, deleteItem, saved }) => {
+  const [ isSelected, toggleSelected ] = useState(false);
+
+  const selectItem = (id, dye, icon, name, type) => {
+    toggleSelected(!isSelected);
+    saveItem(id, dye, icon, name, type);
+  }
 
   return (
-    <div className='item-details' id={id} onClick={() => {saveItem(id, dye, icon, name, type)}}>
+    <div className={`item-details ${isSelected}`} id={id} onClick={() => {selectItem(id, dye, icon, name, type)}}>
       <img src={`https://xivapi.com/${icon}`} className='item-icon' />
       <div className='names'>
         <p className='item-name'>{name}</p>
@@ -28,5 +34,5 @@ Equipment.propTypes = {
   icon: PropTypes.string,
   saveItem: PropTypes.func,
   deleteItem: PropTypes.func,
-  saved: PropTypes.bool
+  saved: PropTypes.bool,
 }
